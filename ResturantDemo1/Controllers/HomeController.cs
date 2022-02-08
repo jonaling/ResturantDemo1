5117442247a1e3f0fc70efbd54ec14b96c2016be
jonaling <jonaling00@gmail.com>
2022-02-08 19:22:49 +1100

    Add project files.

---
+++ ResturantDemo1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResturantDemo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Eat()
+        {
+            ViewBag.Message = "Your pathway to a good time.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
